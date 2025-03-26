894a0e62315bbbbee0e86c2feb5a406f6e78cd56
hadzic20 <hadzic20@itu.edu.tr>
2025-03-27 01:31:16 +0300

    Second day, progress bar almost finished

--- Farm/Assets/ProgressBarHayCode.cs
+++ Farm/Assets/ProgressBarHayCode.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ProgressBarHayCode : MonoBehaviour
 {
     public Slider slider;
     public float time;
+    public int depoCount;
+    public TextMeshProUGUI depotext;
+    public TextMeshProUGUI bartext;
+    public int capacity = 5;
 
     void Start()
     {
         time = 0;
+        depoCount = 0;
+        depotext.text = "" + depoCount;
     }
 
     void Update()
     {
-        if (time <= slider.maxValue) {
+        if (time <= slider.maxValue && depoCount < capacity) {
             time += Time.deltaTime;
+            bartext.text = ((int) (slider.maxValue - time + 1)) + " sn";
             Progress(time);
         }
-        else {
+        else if (depoCount < capacity) {
+            depoCount++;
+            depotext.text = "" + depoCount;
             time = Time.deltaTime;
+        }
+        else {
+            bartext.text = "FULL";
         }
     }
 
